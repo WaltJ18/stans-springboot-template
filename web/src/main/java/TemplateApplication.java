3d50cdb1bd74fd850d0569b1879e11bec0c28bbe
WaltJ18 <stanlywalter18@gmail.com>
2025-08-09 15:46:26 +0700

    POM dependency initials
    Initial insert new POM dependency

---
+++ web/src/main/java/TemplateApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class TemplateApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TemplateApplication.class, args);
+    }
+}
